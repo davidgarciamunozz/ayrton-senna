2bea004a4624ccc743730782a289a15b24a80a2b
David <garxia0710@gmail.com>
2024-12-06 16:17:09 -0500

    fix: og:image relative route

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,14 @@ export const metadata = {
     card: "summary_large_image",
     title: 'Senna Tribute',
     description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
-    images: ['/opengraph-image.png'], // Asegúrate de que la ruta sea correcta
+    images: ['../public/opengraph-image.png'], // Asegúrate de que la ruta sea correcta
   },
   openGraph: {
     title: 'Senna Tribute',
     description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
     images: [
       {
-        url: '/opengraph-image.png', // Ruta de la imagen
+        url: '../public/opengraph-image.png', // Ruta de la imagen
         width: 1200, // Opcional
         height: 630, // Opcional
         alt: 'Ayrton Senna Tribute', // Descripción de la imagen
