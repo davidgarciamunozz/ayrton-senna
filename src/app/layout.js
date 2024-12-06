8d0f9893038b297c7741aad9b0c060db692e9586
David <garxia0710@gmail.com>
2024-12-06 15:20:22 -0500

    layout opengraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Senna Tribute',
   description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
+  openGraph: {
+    title: 'Senna Tribute',
+    description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
+    type: 'website',
+    url: 'https://ayrton-senna-three.vercel.app/',
+    image: './opengraph-image.png',
+    site_name: 'Senna Tribute',
+    locale: 'en_US',
+  },
 }
 
 export default function RootLayout({ children }) {
