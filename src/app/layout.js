85b0e90bd656940b0b5b61fc7d0d2b2eefbe4391
David <garxia0710@gmail.com>
2024-12-06 15:56:50 -0500

    layout opengraph fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,27 @@ export const metadata = {
   title: 'Senna Tribute',
   description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
   twitter: {
-    card: "summary_large_image"
-  }
+    card: "summary_large_image",
+    title: 'Senna Tribute',
+    description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
+    images: ['/opengraph-image.png'], // Asegúrate de que la ruta sea correcta
+  },
+  openGraph: {
+    title: 'Senna Tribute',
+    description: 'A tribute to the legendary Brazilian Formula 1 driver Ayrton Senna.',
+    images: [
+      {
+        url: '/opengraph-image.png', // Ruta de la imagen
+        width: 1200, // Opcional
+        height: 630, // Opcional
+        alt: 'Ayrton Senna Tribute', // Descripción de la imagen
+      },
+    ],
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
