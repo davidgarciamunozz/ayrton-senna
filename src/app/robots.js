6780e3ba0b1bb38195d237d7b22f2d774ceef604
David <garxia0710@gmail.com>
2024-12-07 13:02:28 -0500

    fix: ts changed to js

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://ayrton-senna-three.vercel.app/sitemap.xml",
+  };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,7 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://ayrton-senna-three.vercel.app/',
+        }
+    ]
+}
